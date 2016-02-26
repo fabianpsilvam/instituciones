de787cde01afe11b8ce0a44299143351e20d0cf0
Fabian Silva <fsilva_77@hotmail.com>
2016-02-26 14:31:09 -0500

    Proyecto Web

---
+++ Instituciones/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Instituciones.Startup))]
+namespace Instituciones
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
